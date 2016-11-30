5e8d327fe065909090fe8712ab3a09be12edbbef
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2016-11-29 20:54:15 -0600

    Update

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ var dateController = require('./controllers/dateController');
 
 dateController(app);
 
-app.get('/', function (req, res) {
-    res.send('<html><body><h3>Simple API made for Free Code Camp project. All dates in UTC time.</h3><p>Example: <a href="https://isaacabrahamson-timestamp.herokuapp.com/api/today">https://isaacabrahamson-timestamp.herokuapp.com/api/today</a></p><p>Example: <a href="https://isaacabrahamson-timestamp.herokuapp.com/api/yesterday">https://isaacabrahamson-timestamp.herokuapp.com/api/yesterday</a></p><p>Example: <a href="https://isaacabrahamson-timestamp.herokuapp.com/api/5daysago">https://isaacabrahamson-timestamp.herokuapp.com/api/5daysago</a></p><p>Example: <a href="https://isaacabrahamson-timestamp.herokuapp.com/api/07-05-1999">https://isaacabrahamson-timestamp.herokuapp.com/api/07-05-1999</a></p><p>Example: <a href="https://isaacabrahamson-timestamp.herokuapp.com/api/931132800000">https://isaacabrahamson-timestamp.herokuapp.com/api/931132800000</a></p></body></html>');
+app.set('view engine', 'ejs');
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('index', { heart: String.fromCodePoint(0x1F3B6), pizza: String.fromCodePoint(0x1F355) });
 });
 
 app.listen(PORT);
